788ee21a0b159e9faa66c7f5b15474fc73182a17
Ivan <iv.r@leadsdoit.net>
2024-05-21 13:19:03 +0200

    Added locker

--- lib/fcm/fcm_client.dart
+++ lib/fcm/fcm_client.dart
@@ -1,31 +1,39 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
+import 'package:synchronized/synchronized.dart';
 
 class FCMClient {
   FCMClient._();
 
   static bool isInit = false;
 
-  String? token;
-  RemoteMessage? initialMessage;
+  /// Use synchronized to lock FCM initialization.
+  /// Sometimes init() can be called several times in a row when internet
+  /// connection is unstable
+  final _fCMLock = Lock();
 
   final _firebaseMessaging = FirebaseMessaging.instance;
 
-  Future<void> init() async {
-    if (!isInit) {
-      token = await _firebaseMessaging.getToken();
-      debugPrint('fcm token: $token');
-    }
-    await _firebaseMessaging.requestPermission();
+  String? token;
+  RemoteMessage? initialMessage;
 
-    initialMessage = await _firebaseMessaging.getInitialMessage();
-    if (initialMessage != null) {
-      debugPrint(
-        'initialMessage: title ${initialMessage?.notification?.title}, '
-        'body: ${initialMessage?.notification?.body}',
-      );
-    }
+  Future<void> init() {
+    return _fCMLock.synchronized(() async {
+      if (!isInit) {
+        token = await _firebaseMessaging.getToken();
+        debugPrint('fcm token: $token');
+      }
+      await _firebaseMessaging.requestPermission();
 
-    isInit = true;
+      initialMessage = await _firebaseMessaging.getInitialMessage();
+      if (initialMessage != null) {
+        debugPrint(
+          'initialMessage: title ${initialMessage?.notification?.title}, '
+          'body: ${initialMessage?.notification?.body}',
+        );
+      }
+
+      isInit = true;
+    });
   }
 }
